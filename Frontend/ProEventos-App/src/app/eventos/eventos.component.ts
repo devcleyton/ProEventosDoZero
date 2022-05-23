0583c10c911b0b10408b6b381a250d46868098bf
devcleyton <cleyton.oliveira.dev@gmail.com>
2022-05-23 18:26:49 -0300

    Adição de filtros

--- Frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosfiltrados: any = [];
+  widthImg: number = 100;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filtrolista: string = '';
+
+  public get filtrolista(): string {
+    return this._filtrolista;
+  }
+
+  public set filtrolista(value: string) {
+    this._filtrolista = value;
+    this.eventosfiltrados = this.filtrolista
+      ? this.listafiltrada(this.filtrolista)
+      : this.eventos;
+  }
+
+  listafiltrada(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +42,15 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        (this.eventos = response), (this.eventosfiltrados = this.eventos);
+      },
       (error) => console.log(error)
     );
+  }
+
+  showingImg(): void {
+    this.showImg = !this.showImg;
+    console.log('executando');
   }
 }
